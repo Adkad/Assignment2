646342a4a239315f19e02802b54ead923578f7e8
Adkad <47971858+Adkad@users.noreply.github.com>
2019-11-09 14:24:46 +0530

    Add files via upload

---
+++ Assignment2_1.py
@@ -0,0 +1,8 @@
+from Arithmatic import *
+no1=int(input("Enter numer one :"));
+no2=int(input("Enter numer two :"));
+
+print("Addition is = :", Add(no1,no2));
+print("Subtract is = :", Sub(no1,no2));
+print("Multiplication is = :", Mult(no1,no2));
+print("Division is = :", Div(no1,no2));
